d108c793cd037a940fd55eb8d8276ffd9a35a4dd
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-26 00:17:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  delete 'posts/:id/delete' do
+  delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
     @post.destroy
     erb :delete
